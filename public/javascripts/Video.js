6ad0f5437229d04d1fe8b33db92927eccacb756a
Ariel Shaposhnik <Ariel.Shaposhnik@viacom.com>
2018-03-01 10:43:38 -0500

    new filters

--- public/javascripts/Video.js
+++ public/javascripts/Video.js
@@ -1,29 +1,24 @@
  var img;
  var filterName;
  var dayOrNight;
+ var video;
  var starsDone = false;
 
  var alpha = 0,   /// current alpha value
  delta = 0.1;
 
 var filterList =[{
-    "filterName": "christmasHat",
-    "source":"images/christmasHat.png"
+    "filterName": "fountain",
+    "source":"images/particle_fountain.mp4"  
     },{
-    "filterName": "3dGoggles",
-    "source":"images/glasses2.png"
+    "filterName": "aura",
+    "source":"images/blueaura.mp4"  
     },{
-    "filterName": "DogFace",
-    "source":"images/dog.png"  
+    "filterName": "aura",
+    "source":"images/redaura.mp4"   
     },{
-    "filterName": "Turtle",
-    "source":"images/Turtle.png"  
-    },{
-    "filterName": "Sponge",
-    "source":"images/sponge.png"   
-    },{
-    "filterName": "Patrick",
-    "source":"images/patrick.png"
+    "filterName": "aura",
+    "source":"images/greenaura.mp4"
     },{
     "filterName": "halo",
     "source":"images/halo.png"       
@@ -47,11 +42,16 @@ function startDifTrack(){
           alert("There was an error with your video")
     }
     var commands = {
-        'hi': function() {
+        'switch': function() {
             print("switching")
             var rand = filterList[Math.floor(Math.random() * filterList.length)];
-            img.src = rand.source;
-            filterName = rand.filterName;
+            if(rand.filterName == "halo"){
+                img.src = rand.source;
+                filterName = rand.filterName;
+            } else {
+                video.src = rand.source;
+                filterName = rand.filterName;
+            }
         }
         
     };
@@ -78,66 +78,17 @@ function use2DCanvas(){
     
     img = document.createElement("img");
 
-    img.src = "images/patrick.png"
-    filterName = "Slime"    
-
-    var video = document.createElement("video");
+    video = document.createElement("video");
     video.src = "images/goo.mp4";
     video.addEventListener('loadeddata', function() {
         video.play();  // start playing
         update(); //Start rendering
     });
-
-   //filterName = "Morn";
-
-cc.globalAlpha = .5;
+    
     function drawLoop(){
         requestAnimationFrame(drawLoop);
         //Gets positions
         var positions = ctracker.getCurrentPosition();
-        if (positions){
-            dayOrNight = "Morn"
-        } else {
-            dayOrNight = "nightSky"
-        }
-        if (dayOrNight == "nightSky1"){
-            if(!starsDone){
-                ccbg.clearRect(0, 0, canvasInputbg.width, canvasInputbg.height);
-                $('#canvasbg').css('background', 'linear-gradient(rgba(0,17,30,.5) 30%, rgba(3,61,94,.5))');
-                var xMax = canvasInputbg.width = window.screen.availWidth;
-                var yMax = canvasInputbg.height = window.screen.availHeight;
-                var hmTimes = Math.round(xMax + yMax);	    
-                for(var i=0; i<=hmTimes; i++) {
-                    var randomX = Math.floor((Math.random()*xMax)+1);
-                    var randomY = Math.floor((Math.random()*yMax)+1);
-                    var randomSize = Math.floor((Math.random()*2)+1);
-                    var randomOpacityOne = Math.floor((Math.random()*9)+1);
-                    var randomOpacityTwo = Math.floor((Math.random()*9)+1);
-                    var randomHue = Math.floor((Math.random()*360)+1);
-                    if(randomSize>1) {
-                    ccbg.shadowBlur = Math.floor((Math.random()*15)+5);
-                    ccbg.shadowColor = "white";
-                    }
-                    ccbg.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
-                    ccbg.fillRect(randomX, randomY, randomSize, randomSize);
-                }
-                starsDone = true;
-            } 
-        } else if (dayOrNight == "Morn1"){
-            starsDone = false;   
-            ccbg.clearRect(0, 0, canvasInputbg.width, canvasInputbg.height);
-            $('#canvasbg').css('background', 'linear-gradient(rgba(237, 235, 154,.5), rgba(142, 230, 242,.5))');
-            ccbg.globalCompositeOperation = 'destination-out';
-            ccbg.fillStyle = 'rgba(0, 0, 0, .1)';
-            ccbg.fillRect( 0, 0, canvasInputbg.width, canvasInputbg.height );
-            ccbg.globalCompositeOperation = 'lighter';
-            
-            /* Sun */
-            ccbg.beginPath();
-            ccbg.arc( 100 + Math.random(), 75 + Math.random() , 50 + Math.random() * 5, 0, Math.PI * 2);
-            ccbg.fillStyle = 'rgba(255, 249, 21, 0.8)';
-            ccbg.fill();
-        }
         if (filterName == "3dGoggles") {
             cc.clearRect(0, 0, canvasInput.width, canvasInput.height);
             //Gets the slope for rotation
